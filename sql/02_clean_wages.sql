161d1cec183912a895735b7d4cc1334197f2a52a
timmylem23 <timmylem01@gmail.com>
2026-01-19 22:59:29 -0800

    Add SQL cleaning script for wages

---
+++ sql/02_clean_wages.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS clean_wages;
+
+CREATE TABLE clean_wages AS
+SELECT
+	STR_TO_DATE(observation_date, '%Y-%m-%d') AS date,
+	AHETPI AS VALUE
+FROM AHETPI
+WHERE AHETPI IS NOT NULL
+ORDER BY date;
